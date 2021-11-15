536a5870a7f6cf8d771ab10406ddd27fb8a4710c
EpsIotaPi <1037272365@qq.com>
2021-11-15 16:35:09 +0800

    增加了model组件，写入模型内容

---
+++ model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+
+class myModel(nn.Module):
+    def __init__(self):
+        super(myModel, self).__init__()
+        pass
+
+    def forward(self):
+        pass
